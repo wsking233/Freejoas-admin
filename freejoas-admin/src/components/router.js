5f0abdd1a83570778671d74488353994d81996a0
Shuai Wang <wsking233@gmail.com>
2024-03-25 23:53:54 +1300

    add private route, check is token exists

--- freejoas-admin/src/components/router.js
+++ freejoas-admin/src/components/router.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import CookieManager from './cookieManager';
 import Login from '../pages/login';
 import Dashboard from '../pages/dashboard';
 
 function Router() {
+
+  const PrivateRoute = ({ element }) => {
+    const token = CookieManager().getCookie('token');
+    
+    return token ? element : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/">
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/login" element={<Login />} />
 
-                {/* 404 Page */}
-                <Route path="*" element={<h1>Page Not Found</h1>} />
-            </Route>
-        </Routes>
+        {/* 404 Page */}
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- freejoas-admin/src/pages/dashboard.js
+++ freejoas-admin/src/pages/dashboard.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CookieManager from "../components/cookieManager";
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        console.log('logout');
+        const cookieManager = CookieManager();
+        cookieManager.removeCookie('token');
+        navigate('/login');
+    }
+
     return (
         <div>
             <h1>Dashboard pages</h1>
+
+            <div>
+                <form onSubmit={handleLogout}>
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
+
         </div>
     );
 }
